5a11db10d46bc36b8a3370d2f0cb71d8637d7799
farisamirmudin <farisamirmudin@gmail.com>
2022-07-31 11:46:06 +0200

    update script.sh

--- script.sh
+++ script.sh
@@ -3,11 +3,11 @@ roslaunch turtlebot3_bringup turtlebot3_robot.launch
 
 # script.sh
 ## Straight line
-rostopic pub -r 10 /cmd_vel geometry_msgs/Twist "{linear: {x: 0.2, y: 0.0, z: 0.0}, angular: {x: 0.2, y: 0.0, z: 0.0}}" & 
+rostopic pub -r 10 /cmd_vel geometry_msgs/Twist "{linear: {x: 0.2, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}" & 
 sleep 15 
-rostopic pub --once /cmd_vel geometry_msgs/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.2, y: 0.0, z: 0.0}}" & # Stop
+rostopic pub --once /cmd_vel geometry_msgs/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}" & # Stop
 
 ## Circular Motion
-rostopic pub -r 10 /cmd_vel geometry_msgs/Twist "{linear: {x: 0.24, y: 0.0, z: 0.0}, angular: {x: 0.2, y: 0.0, z: -0.4}}" &
+rostopic pub -r 10 /cmd_vel geometry_msgs/Twist "{linear: {x: 0.24, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: -0.4}}" &
 sleep 20
-rostopic pub --once /cmd_vel geometry_msgs/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.2, y: 0.0, z: 0.0}}" & # Stop
+rostopic pub --once /cmd_vel geometry_msgs/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}" & # Stop
